1d6228eaa54dd7c8e383a90c13bdbd8aafab81e0
laprn <reg@dymcom.net>
2021-07-16 09:56:22 +0900

    correspond ajax

--- app.py
+++ app.py
@@ -1,14 +1,21 @@
-from flask import Flask
+from flask import Flask, render_template, request
+from flask.wrappers import Request
 from fetchhtmlinfo import fetch_header
 
 app = Flask(__name__)
 
 @app.route('/')
 def greeting():
-    return "<code>GET /url:https://example.com</code>"
+    return render_template('index.html')
 
 @app.route('/<path:url>')
-def display_json(url):
+def display_json_curl(url):
+    result = fetch_header.fetch_title(url)
+    return result
+
+@app.route('/search')
+def display_json_form():
+    url = request.args.get('url')
     result = fetch_header.fetch_title(url)
     return result
 
