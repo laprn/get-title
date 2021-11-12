19666a286b1d4007b396c7387c398e4c88d086a0
laprn <reg@dymcom.net>
2021-11-12 18:11:54 +0900

    flask flash

--- app.py
+++ app.py
@@ -1,11 +1,12 @@
-from flask import Flask, render_template, request
+from flask import Flask, render_template, request, flash, redirect, url_for
 from flask.wrappers import Request
 from fetchhtmlinfo import fetch_header
 
 app = Flask(__name__)
+app.secret_key = b'_5#y2L"F4Q8z\n\xec]/'
 
 @app.route('/')
-def greeting():
+def index():
     return render_template('index.html')
 
 @app.route('/<path:url>')
@@ -17,7 +18,9 @@ def display_json_curl(url):
 def display_json_form():
     url = request.args.get('url')
     result = fetch_header.fetch_title(url)
-    return result
+    flash(result)
+    return redirect(url_for('index'))
+    # return result
 
 if __name__ == '__main__':
     app.run()
